355f3fc0bc416ca6d1a58ebdcc326a167b7a7cc5
nauman-riaz <nauman.riaz@10pearls.com>
2024-06-26 17:09:05 +0500

    commit from dev to prod

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+// import "./App.css";
+// import store from "./App/Store";
+// import { Provider } from "react-redux";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(
+//   <React>
+//     <BrowserRouter>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </BrowserRouter>
+//   </React>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css";
 import store from "./App/Store";
@@ -9,11 +28,9 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
